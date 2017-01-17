a308b43cfa860c065fb4c233f69aca1529968e7c
joseanu <joseanu@gmail.com>
2017-01-17 22:19:38 +0000

    Cambio de toda la estructura. Ahora con webpack!

--- _assets/js/util.js
+++
@@ -1,4 +0,0 @@
-function $id(id) {
-	return document.getElementById(id);
-};
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,52 @@
-const child = require('child_process');
-const browserSync = require('browser-sync').create();
+// Modificado de https://github.com/mmistakes/made-mistakes-jekyll/blob/master/gulpfile.js
+
+'use strict';
 
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const svgmin = require('gulp-svgmin');
 const svgstore = require('gulp-svgstore');
 const cheerio = require('gulp-cheerio');
-const responsive = require('gulp-responsive');
 const rename = require('gulp-rename');
 const purify = require('gulp-purifycss');
-//const changed = require('gulp-changed');
-
-const siteRoot = '_site';
 
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+const requireDir = require('require-dir');
+const tasks = requireDir('./gulp/tasks', {recurse: true}); // eslint-disable-line
 
-gulp.task('jekyll', () => {
-    const jekyll = child.spawn('bundle', ['exec',
-        'jekyll',
-        'build',
-        '--watch',
-        '--incremental',
-        '--config',
-        '_config.yml,_config.dev.yml'
-    ]);
+// include paths file
+var paths      = require('./gulp/paths');
 
-    const jekyllLogger = (buffer) => {
-        buffer.toString()
-            .split(/\n/)
-            .forEach((message) => gutil.log('Jekyll: ' + message));
-    };
+// 'gulp images' -- copies, builds, and then copies it again
+gulp.task('images', gulp.series('images:noresize', 'images:productos', 'images:sucursales', 'images:svg'));
 
-    jekyll.stdout.on('data', jekyllLogger);
-    jekyll.stderr.on('data', jekyllLogger);
-});
+// 'gulp scripts' -- genera los JS
+gulp.task('scripts', gulp.parallel('scripts:webpack'));
 
-gulp.task('serve', () => {
-    browserSync.init({
-        files: [siteRoot + '/**'],
-        host: process.env.IP,
-        port: process.env.PORT,
-        ui: { port: 8081 },
-        server: {
-            baseDir: siteRoot
-        }
-    });
-});
+// 'gulp assets' -- removes assets and rebuilds them
+// 'gulp assets --prod' -- same as above but with production settings
+gulp.task('assets', gulp.series(
+  gulp.parallel('styles', 'scripts'),
+  gulp.series('images', 'copy:assets', 'copy:inline')
+));
 
-gulp.task('servir', ['jekyll', 'serve']);
+// 'gulp clean' -- removes temporary and built files
+gulp.task('clean', gulp.parallel('clean:assets', 'clean:dist', 'clean:site'));
 
+// 'gulp inject' -- injects CSS and JS into includes
+gulp.task('inject', gulp.parallel('inject:css', 'inject:scripts'));
 
+// 'gulp php' -- Copa archivos php a _site
+gulp.task('php', gulp.parallel('copy:phplib', 'copy:php'));
+
+// 'gulp build:site' -- copies, builds, and then copies it again
+gulp.task('build:site', gulp.series('copy:tmp', 'inject', 'copy:productos', 'site', 'clean:productos', 'copy:site'));
+
+// 'gulp build' -- same as 'gulp' but doesn't serve site
+// 'gulp build --prod' -- same as above but with production settings
+gulp.task('build', gulp.series('clean', 'assets', 'build:site', 'html', 'php', 'copy:favicons'));
+
+gulp.task('default', gulp.series('build', gulp.parallel('watch', 'server')));
 
 gulp.task('iconos', function () {
   return gulp.src('./src/assets/icons/*')
@@ -72,7 +66,6 @@ gulp.task('iconos', function () {
     .pipe(gulp.dest('./_assets/images/'));
 });
 
-
 gulp.task('productos', function () {
   return gulp.src('./src/assets/productos/*')
     .pipe(svgmin({
@@ -90,7 +83,6 @@ gulp.task('productos', function () {
     .pipe(rename('iconos-productos.svg'))
     .pipe(gulp.dest('./_assets/images/productos/'));
 });
-
 
 gulp.task('mapa', function () {
   const genMapa = child.spawn('node', ['./src/assets/mapa/mapa-home.js']);
@@ -121,143 +113,4 @@ gulp.task('mapa', function () {
         .pipe(gulp.dest('./_assets/images/productos/'));
     }
   });
-
-});
-
-const IMG_SRC = './src/assets/img_sucursales/*.jpg';
-const IMG_DEST = './_assets/images/sucursales/';
-
-gulp.task('images', function () {
-  return gulp.src(IMG_SRC)
-//    .pipe(changed(IMG_DEST))
-    .pipe(responsive({
-      '*.jpg': [{
-        width: 200,
-        rename: {
-          suffix: '-200px'
-        }
-      },{
-        width: 300,
-        rename: {
-          suffix: '-300px'
-        }
-      },{
-        width: 400,
-        rename: {
-          suffix: '-400px'
-        }
-      },{
-        width: 600,
-        rename: {
-          suffix: '-600px'
-        }
-      },{
-        width: 800,
-        rename: {
-          suffix: '-800px'
-        },
-        withoutEnlargement: true
-      },{
-        width: 200,
-        rename: {
-          suffix: '-200px',
-          extname: '.webp'
-        }
-      },{
-        width: 300,
-        rename: {
-          suffix: '-300px',
-          extname: '.webp'
-        }
-      },{
-        width: 400,
-        rename: {
-          suffix: '-400px',
-          extname: '.webp'
-        }
-      },{
-        width: 600,
-        rename: {
-          suffix: '-600px',
-          extname: '.webp'
-        }
-      },{
-        width: 800,
-        rename: {
-          suffix: '-800px',
-          extname: '.webp'
-        },
-        withoutEnlargement: true
-      }]
-    }, {
-      // Global configuration for all images
-      // The output quality for JPEG, WebP and TIFF output formats
-      quality: 80,
-      // Use progressive (interlace) scan for JPEG and PNG output
-      progressive: true,
-      // Zlib compression level of PNG output format
-      compressionLevel: 6,
-      // Strip all metadata
-      withMetadata: false,
-    }))
-    .pipe(gulp.dest(IMG_DEST));
-});
-
-gulp.task('css', function() {
-  return gulp.src('./_site/assets/main.css')
-    .pipe(purify(['./_site/**/*.js', './_site/**/*.html']))
-    .pipe(gulp.dest('./src/'));
-});
-
-
-var critical = require('critical').stream;
-// Generate & Inline Critical-path CSS
-gulp.task('critical', function () {
-    return gulp.src('_site/*.html')
-        .pipe(critical({base: '_site/', inline: true}))
-        .pipe(gulp.dest('_site'));
-});
-
-const IMG_PROD_SRC = './src/assets/img_productos/**/*.jpg';
-const IMG_PROD_DEST = './img/productos/';
-
-gulp.task('prodImg', function () {
-  return gulp.src(IMG_PROD_SRC)
-    .pipe(responsive({
-      '**/*.jpg': [{
-        width: 25,
-        rename: {
-          prefix: 'lqip-'
-        }
-      },{
-        width: 200,
-        rename: {
-          suffix: '-200px'
-        }
-      },{
-        width: 300,
-        rename: {
-          suffix: '-300px'
-        }
-      },{
-        width: 400,
-        rename: {
-          suffix: '-400px'
-        }
-      },{
-        width: 600,
-        rename: {
-          suffix: '-600px'
-        }
-      }]
-    }, {
-      // Global configuration for all images
-      // The output quality for JPEG, WebP and TIFF output formats
-      quality: 80,
-      // Use progressive (interlace) scan for JPEG and PNG output
-      progressive: true,
-      // Strip all metadata
-      withMetadata: false,
-    }))
-    .pipe(gulp.dest(IMG_PROD_DEST));
 });
