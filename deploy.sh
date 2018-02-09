242bf8b4b9675ea8c48671295f8fb83d4aa8e31a
joseanu <joseanu@gmail.com>
2018-02-09 18:00:20 +0000

    actualizar dependencias modificar el deploy de JR

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ LOCALPATH='./_jrecotec/_site/'
 FTPUSER=$1
 FTPHOST='66.7.195.240'
 
-lftp -c "set ftp:list-options -a; open ftp://$FTPUSER@$FTPHOST; lcd $LOCALPATH; cd $REMOTEPATH; mirror --reverse --delete --use-cache --verbose --allow-chown --allow-suid --no-umask --parallel=2"
+lftp -c "set ftp:list-options -a; open ftp://$FTPUSER@$FTPHOST; lcd $LOCALPATH; cd $REMOTEPATH; mirror --only-newer --reverse --delete --use-cache --verbose --allow-chown --allow-suid --no-umask --parallel=2"
